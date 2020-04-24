c88b1f8d42610a2564d0a376a4ff4a8cad48a282
nchkonia <nickchkonia96@gmail.com>
2020-04-24 13:40:38 -0500

    basic routing setup

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('')
+    path('', views.home, name='blog-home'),
+    path('about/', views.about, name='blog-about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -8,3 +8,6 @@ def home(request):
     '''
     return HttpResponse('<h1>Home</h1>')
 
+
+def about(request):
+    return HttpResponse('<h1>About</h1>')
