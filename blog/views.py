50de25cf41ae76dcd633eaec76f7019b1e7e6172
nchkonia <nickchkonia96@gmail.com>
2020-04-25 12:48:34 -0500

    did basic routing for blog shell

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,32 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+
+posts = [
+    {
+        'author': 'SpidersForEyes',
+        'title': 'Blog Post 1',
+        'content': 'First post content', 
+        'date_posted': 'April 23, 2020'
+    },
+    {
+        'author': 'SpidersForEyes',
+        'title': 'Blog Post 2',
+        'content': 'Second post content', 
+        'date_posted': 'April 24, 2020'
+    }
+]
 
 # Create your views here.
 def home(request):
     '''
     routes traffic to home page
     '''
-    return HttpResponse('<h1>Home</h1>')
+    context = {
+        'posts': posts,
+        'title': 'სახლი',
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>About</h1>')
+    return render(request, 'blog/about.html', {'title': 'about'})
