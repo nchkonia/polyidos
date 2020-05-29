b52cf0d0d78afe5f903dbbb2a010e822aa2154f7
nchkonia <nickchkonia96@gmail.com>
2020-05-29 18:19:27 +0400

    added profile and images

--- polyidos/urls.py
+++ polyidos/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 import users.views
 
 urlpatterns = [
@@ -27,3 +29,6 @@ urlpatterns = [
     path('profile/', users.views.profile, name='profile'),
     
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
